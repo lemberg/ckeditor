022c349c529a1bdb7803e3f7e985624ae243bcbf
Taras Kruts <taras.kruts@lemberg.co.uk>
2014-05-16 15:18:16 +0300

    Updated CKEditor to version 4.4.0.

--- build-config.js
+++ build-config.js
@@ -1,14 +1,25 @@
-﻿
-/**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+﻿/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 /**
  * This file was added automatically by CKEditor builder.
- * You may re-use it at any time at http://ckeditor.com/builder to build CKEditor again.
- * 
- * NOTE: 
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/4264b901c3ed4843d0aa65ceef514aa4
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/4264b901c3ed4843d0aa65ceef514aa4
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
  *    This file is not used by CKEditor, you may remove it.
  *    Changing this file will not change your CKEditor configuration.
  */
@@ -24,46 +35,27 @@ var CKBUILDER_CONFIG = {
 		'.mailmap'
 	],
 	plugins : {
+		'about' : 1,
 		'basicstyles' : 1,
 		'clipboard' : 1,
-		'toolbar' : 1,
+		'div' : 1,
+		'divarea' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
 		'floatingspace' : 1,
-		'wysiwygarea' : 1,
+		'image' : 1,
 		'indentlist' : 1,
 		'link' : 1,
 		'list' : 1,
-		'undo' : 1,
-		'dialog' : 1,
-		'dialogui' : 1,
-		'button' : 1,
-		'indent' : 1,
-		'fakeobjects' : 1,
-		'blockquote' : 1,
-		'panel' : 1,
-		'floatpanel' : 1,
-		'menu' : 1,
-		'contextmenu' : 1,
-		'listblock' : 1,
-		'richcombo' : 1,
-		'format' : 1,
-		'justify' : 1,
 		'liststyle' : 1,
+		'symbol' : 1,
 		'table' : 1,
-		'sourcearea' : 1,
-		'image' : 1,
-		'maximize' : 1,
-		'removeformat' : 1,
-		'showborders' : 1,
-		'popup' : 1,
-		'filebrowser' : 1,
-		'stylescombo' : 1,
-		'htmlwriter' : 1,
-		'div' : 1,
-		'tabletools' : 1
+		'tabletools' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'wysiwygarea' : 1
 	},
 	languages : {
-		'en' : 1,
+		'en' : 1
 	}
 };
